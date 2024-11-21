ff4bcf457ec14d2462810930623b4ffc60eab24a
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2024-11-21 12:33:39 +0000

    Refine password_auth checks to be clearer.

--- cypress/e2e/password_auth/spec.cy.ts
+++ cypress/e2e/password_auth/spec.cy.ts
@@ -5,22 +5,42 @@ describe('Password Auth', () => {
     runTestServer();
   });
 
-  it('should fail to login with wrong credentials', () => {
-    cy.get("input[name='email']").type('user');
-    cy.get("input[name='password']").type('user');
-    cy.get("button[type='submit']").click();
-    cy.get('.MuiAlert-message').should('exist');
+  describe('when unauthenticated', () => {
+    it('should redirect to login dialog', () => {
+      cy.visit('/');
+      cy.location('pathname').should('eq', '/login');
+      cy.get("input[name='email']").should('exist');
+      cy.get("input[name='password']").should('exist');
+    });
+
+    describe('visiting the /login', () => {
+      beforeEach(() => {
+        cy.visit('/login');
+      })
+
+      it('should fail to login with wrong credentials', () => {
+        cy.get("input[name='email']").type('user');
+        cy.get("input[name='password']").type('user');
+        cy.get("button[type='submit']").click();
+        cy.get('.MuiAlert-message').should('exist');
+        cy.get('ol.toast');
+      });
+
+      it('should be able to login with correct credentials', () => {
+        cy.get("input[name='email']").type('admin');
+        cy.get("input[name='password']").type('admin');
+        cy.get("button[type='submit']").click();
+        cy.get('.step').eq(0).should('contain', 'Hello admin');
+
+        cy.reload();
+        cy.get("input[name='email']").should('not.exist');
+        cy.get('.step').eq(0).should('contain', 'Hello admin');
+      });
+
+    })
+
+
+
   });
 
-  it('should be able to login with correct credentials', () => {
-    cy.visit('/');
-    cy.get("input[name='email']").type('admin');
-    cy.get("input[name='password']").type('admin');
-    cy.get("button[type='submit']").click();
-    cy.get('.step').eq(0).should('contain', 'Hello admin');
-
-    cy.reload();
-    cy.get("input[name='email']").should('not.exist');
-    cy.get('.step').eq(0).should('contain', 'Hello admin');
-  });
 });
