0740a9ce7072d4b107f37aea638633fa0fe13b1f
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2024-11-27 22:15:15 +0000

    In useUser(), don't retry every 5s on 401.

--- libs/react-client/src/auth/user.ts
+++ libs/react-client/src/auth/user.ts
@@ -13,7 +13,19 @@ export const useUser = () => {
   const { handleSetAccessToken } = useTokenManagement();
 
   const { data: userData, mutate: mutateUserData } = useApi<IUser>(
-    cookieAuth ? '/user' : null
+    cookieAuth ? '/user' : null,
+    {
+      onErrorRetry: (...args) => {
+        const [err, _, config] = args;
+
+        // Don't do automatic retry for 401 - it just means we're not logged in (yet).
+        // TODO: Consider setUser(null) if (user)
+        if (err.status === 401) return;
+
+        // Fall back to default behavior.
+        return config.onErrorRetry(...args);
+      }
+    }
   );
 
   // setUser, only once (prevents callback loops).
